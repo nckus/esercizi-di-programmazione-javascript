f0ae4547b3bc6a33e4ec3733a77ff884fcc17fc5
nckus <57502236+nckus@users.noreply.github.com>
2023-04-29 22:39:11 +0200

    e uno è fatto
    fatemi sapere se può essere fatto più semplice.
    grazie

--- esercizi-stringhe-numeri/somma-e-media.js
+++ esercizi-stringhe-numeri/somma-e-media.js
@@ -5,6 +5,17 @@
   Esempio:
     Input: a = 1, b = 2, c = 3, d = 4, e = 5
     Output: somma = 15, media = 3
-  
+
   http://www.imparareaprogrammare.it
 */
+var a = 8;
+var b = 25;
+var c = 89;
+var d = 8;
+var e = 6;
+
+sum = a + b + c + d + e;
+average = sum/5;
+
+console.log(sum);
+console.log(average);
